06b5c2061003514500612671688b035424d7d521
Catalina Castillo <catalina.castillo19c@gmail.com>
2026-08-08 11:25:00 -0400

    debug env vars

--- flask_app/config/mysqlconnection.py
+++ flask_app/config/mysqlconnection.py
@@ -3,21 +3,19 @@ import pymysql.cursors
 from dotenv import load_dotenv
 
 load_dotenv()
+# --- DEBUG TEMPORAL ---
+print("DEBUG DB_HOST =", repr(os.environ.get('DB_HOST')))
+print("DEBUG DB_PORT =", repr(os.environ.get('DB_PORT')))
+print("DEBUG DB_USER =", repr(os.environ.get('DB_USER')))
+# -----------------------
 
 class MySQLConnection:
     def __init__(self, db):
-        host = os.environ.get('MYSQLHOST') or os.environ.get('DB_HOST') or 'localhost'
-        port = int(os.environ.get('MYSQLPORT') or os.environ.get('DB_PORT') or 3306)
-        user = os.environ.get('MYSQLUSER') or os.environ.get('DB_USER') or 'root'
-        password = os.environ.get('MYSQLPASSWORD') or os.environ.get('DB_PASSWORD') or ''
-
-        print(f"Intentando conexion a MySQL en Host: {host}, Port: {port}, User: {user}")
-
         connection = pymysql.connect(
-            host=host,
-            port=port,
-            user=user,
-            password=password,
+            host=os.environ.get('DB_HOST'),
+            port=int(os.environ.get('DB_PORT', 3306)),
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASSWORD'),
             db=db,
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor,
@@ -29,7 +27,7 @@ class MySQLConnection:
         with self.connection.cursor() as cursor:
             try:
                 query = cursor.mogrify(query, data)
-                print("query", query)
+                print("Ejecutando Query:", query)
                 cursor.execute(query, data)
                 if query.lower().find("insert") >= 0:
                     self.connection.commit()
@@ -40,7 +38,7 @@ class MySQLConnection:
                 else:
                     self.connection.commit()
             except Exception as e:
-                print("error:", e)
+                print("error", e)
                 return False
             finally:
                 self.connection.close()
