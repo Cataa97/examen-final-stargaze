901cdf98579d04dfd398da224e55438291dc0a11
Catalina Castillo <catalina.castillo19c@gmail.com>
2026-08-08 10:34:35 -0400

    edit serverpy

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
-import os
 from flask_app import app
 from flask_app.controllers import usuarios_controller, publicaciones_controller
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
