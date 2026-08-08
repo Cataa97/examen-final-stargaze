32408e5102b2d4875022034f4ece8d71c3ed0721
Catalina Castillo <catalina.castillo19c@gmail.com>
2026-08-08 10:27:32 -0400

    host y puerto dinámico

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
+import os
 from flask_app import app
 from flask_app.controllers import usuarios_controller, publicaciones_controller
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
